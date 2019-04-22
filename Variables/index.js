35b11cbbc19109ed9bf6594451103010e2dfdcff
Israel Calderon <icalderond@outlook.com>
2019-04-21 23:46:37 -0500

    Showing changes of variables

---
+++ Variables/index.js
@@ -0,0 +1,11 @@
+var nombre = 'Israel', apellido = 'Calderón'
+var edad=28
+
+edad='28 años'
+
+console.log("Hola "+nombre+' '+apellido)
+console.log("Tengo "+edad);
+
+//JS es un lenguaje debilmente tipado
+
+var peso = 75
