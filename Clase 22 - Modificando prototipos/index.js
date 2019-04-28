a7dd68f123168f8b969ea582e711d7c142495af9
Israel Calderon <icalderond@outlook.com>
2019-04-28 09:00:47 -0500

    Modificando prototipos

---
+++ Clase 22 - Modificando prototipos/index.js
@@ -0,0 +1,22 @@
+//Prototipo Persona
+function Persona(nombre,apellido,altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.edad = 20 //Valor definido
+  this.altura = altura
+}
+
+//Siempre definir los prototipos arriba para no tener errores de definicion
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+};
+Persona.prototype.soyAlto = () => this.altura > 1.8;
+
+var sacha = new Persona('Sacha','Lifszyc',1.72)
+var ericka = new Persona('Ericka','Luna',1.65)
+var arturo = new Persona('Arturo', 'Martinez',1.89)
+
+//Esto nos regresa todo falso en consola
+sacha.soyAlto()
+ericka.soyAlto()
+arturo.soyAlto()
